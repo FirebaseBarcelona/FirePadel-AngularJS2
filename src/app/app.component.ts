dc0918a2476b3931416de4ca7b1ef156ca78db1c
Jeffarese <jeffarese@gmail.com>
2016-09-04 23:24:40 +0200

    Added Google login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AngularFire, AuthProviders, AuthMethods} from 'angularfire2';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  private user: any = {};
+
+  constructor(public af: AngularFire) {
+    this.observeLogin();
+  }
+
+  observeLogin() {
+    console.log('Observe');
+    this.af.auth.subscribe(user => {
+      console.log(`--> ${user}`);
+      if (user) {
+        this.user = user;
+      } else {
+        this.user = null;
+      }
+    });
+  }
+
+  public login(): void {
+    this.af.auth.login({
+      provider: AuthProviders.Google,
+      method: AuthMethods.Popup
+    });
+  }
+
+  public logout(): void {
+    this.af.auth.logout();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {AppComponent} from './app.component';
+import {CourtComponent} from './court/court.component';
+import {ChatWindowComponent} from './chat-window/chat-window.component';
+import {ChatMessageComponent} from './chat-message/chat-message.component';
+import {MdToolbarModule} from '@angular2-material/toolbar';
+import {MdCardModule} from '@angular2-material/card';
+import {MdIconModule, MdIconRegistry} from '@angular2-material/icon';
+import {MdButtonModule} from '@angular2-material/button';
+import {AngularFireModule} from 'angularfire2';
+import * as firebase from 'firebase';
 
-import { AppComponent } from './app.component';
-import { CourtComponent } from './court/court.component';
-import { ChatWindowComponent } from './chat-window/chat-window.component';
-import { ChatMessageComponent } from './chat-message/chat-message.component';
-import {MdToolbarModule} from "@angular2-material/toolbar";
-import {MdCardModule} from "@angular2-material/card";
-import {MdIconModule, MdIconRegistry} from "@angular2-material/icon";
-import {MdButtonModule} from "@angular2-material/button";
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBWTxmNMGCyjdZtVnYpA6XrQgxPWiQo2nc',
+  authDomain: 'padel-109d5.firebaseapp.com',
+  databaseURL: 'https://padel-109d5.firebaseio.com',
+  storageBucket: 'padel-109d5.appspot.com',
+};
 
 @NgModule({
   declarations: [
@@ -26,11 +35,13 @@ import {MdButtonModule} from "@angular2-material/button";
     MdCardModule,
     MdIconModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [
     MdIconRegistry
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
